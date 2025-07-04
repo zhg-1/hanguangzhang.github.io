4319cf69b5eb14530266117ff25367335cf9d544
zhanghanguang-git <mr_zhanghanguang@163.com>
2025-07-04 22:37:45 +0800

    fix(webpack):(config) add  html-wecpack-plus  and devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackplugein = require("html-webpack-plugin");
 module.exports = {
   mode: "development", //模式配置
   devtool: "inline-source-map", //查看打包后的源代码
@@ -6,8 +7,9 @@ module.exports = {
   output: {
     //配置打包后输出的文件名及目录
     path: path.resolve(__dirname, "dist"), //打包后存放目录,__dirname为一级目录，dist为二级目录
-    // filename: "[name].[contenthash]", //打包后文件名,name可以写固定字符，默认为“main”
-    filename: "main.js", //打包后文件名,name可以写固定字符，默认为“main”
+    filename: "[name].[contenthash].js", //打包后文件名,name可以写固定字符，默认为“main”
+    // filename: "main.js", //打包后文件名,name可以写固定字符，默认为“main”
+    clean: true,
   },
   module: {
     rules: [
@@ -17,5 +19,14 @@ module.exports = {
         loader: "babel-loader",
       },
     ],
+  },
+  plugins: [
+    new HtmlWebpackplugein({
+      title: "最强AI", //支持配置标题
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+  devServer: {
+    static: "./dist", //指定服务要从哪里加载代码
   },
 };
